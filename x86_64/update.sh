13666f1d671764cc66717a1c105ea45ac8dda871
TeArch Linux <tearch.linux@gmail.com>
2020-06-23 15:42:00 +0000

    Added 'calamares' package.

--- x86_64/update.sh
+++ x86_64/update.sh
@@ -5,3 +5,4 @@
 ##################################################################################################################
 
 repo-add tearch_repo.db.tar.gz *.pkg.tar.xz
+repo-add tearch_repo.db.tar.gz *.pkg.tar.zst
