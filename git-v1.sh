5eb7f76257c9ad0e4830cd5ba007851cd0d08f77
Efe Çetin <efectn@6tel.net>
2021-04-30 16:25:20 +0300

    Remove sig files for backwards compatibility.

--- git-v1.sh
+++ git-v1.sh
@@ -8,9 +8,8 @@ git pull
 echo "What is your GPG key? (empty: none)"
 read gpg_key
 
+rm x86_64/*.pkg.tar.*sig
 if ! [[ $gpg_key == "none" ]]; then
-    rm x86_64/*.pkg.tar.*sig
-
     for i in $(ls --ignore="*.sig" --ignore="*.sh" --ignore="*.db*" --ignore="*.files*" x86_64/); do
         gpg --local-user ${gpg_key} --yes --detach-sign --use-agent "x86_64/${i}"
     done
